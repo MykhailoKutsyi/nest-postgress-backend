54b0c2335da4a31f9ad82dcf409f25bdab1abca2
MykhailoKutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-10-19 14:54:24 +0300

    move to HEROKU online DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         database: configService.get('DB_NAME'),
         autoLoadEntities: true,
         synchronize: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       }),
       inject: [ConfigService],
     }),
